66151e8c7e3d20d2eb2549b4a09579e27599c7d6
Sinantosun <sinan_tosun4141@hotmail.com>
2024-11-13 14:01:24 +0300

    ---

---
+++ Frontends/MultiShop.WebUI/ViewComponents/CatalogComponents/CatalogFeatureProductComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.CatalogComponents
+{
+    public class CatalogFeatureProductComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
